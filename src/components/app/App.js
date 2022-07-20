b4f2e05c7d1d12dbf6c9fb9a93772bc6e16aad9b
Rostyslav <bovonenko@icloud.com>
2022-07-20 18:49:53 +0200

    added React Router v5 to my project

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,49 +1,25 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {MainPage, ComicsPage} from '../pages';
 import AppHeader from "../appHeader/AppHeader";
-import AppBanner from '../appBanner/AppBanner';
-import ComicsList from '../comicsList/ComicsList';
-import SingleComic from "../singleComic/SingleComic";
-// import RandomChar from "../randomChar/RandomChar";
-// import CharList from "../charList/CharList";
-// import CharInfo from "../charInfo/CharInfo";
-// import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-
-// import decoration from '../../resources/img/vision.png';
 
 const App = () => {
-    // const [selectedChar, setChar] = useState(null);
-    const [selectedComic, setComic] = useState(null);
-
-    const onComicSelected = (id) => {
-        setComic(id)
-    }
-
-    // const onCharSelected = (id) => {
-    //     setChar(id)
-    // }
-
+    
     return (
-        <div className="app">
-            <AppHeader/>
-            <main>
-                <AppBanner/>
-                <ComicsList onComicSelected={onComicSelected}/>
-                <SingleComic selectedComic={selectedComic}/>
-                {/* <ErrorBoundary>
-                    <RandomChar/>
-                </ErrorBoundary>
-                <div className="char__content">
-                    <ErrorBoundary>
-                        <CharList onCharSelected={onCharSelected}/>
-                    </ErrorBoundary>
-                    
-                    <ErrorBoundary>
-                        <CharInfo charId={selectedChar}/>
-                    </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/> */}
-            </main>
-        </div>
+        <Router>
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    <Switch>
+                        <Route exact path="/" >
+                            <MainPage/>
+                        </Route>
+                        <Route exact path="/comics">
+                            <ComicsPage/>
+                        </Route>
+                    </Switch>
+                </main>
+            </div>
+        </Router>
     )
 }
 
